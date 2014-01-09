48c7df2576b35fdd13ce1fd5fae558040e0e70df
Elton Oliveira <eltonknoxville@hotmail.com>
2014-01-09 09:01:56 -0400

    Alteracoes configurações iniciais
    Alteracoes configurações iniciais

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -5,11 +5,13 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 class Index extends CI_Controller {
 
     function __construct() {
-    	
+
         parent::__construct();
     }
 
     function index() {
+
+    	$this->load>view('index');
         
     }
 }
